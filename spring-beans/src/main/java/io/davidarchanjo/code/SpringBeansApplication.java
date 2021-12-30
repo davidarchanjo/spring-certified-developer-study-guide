b0912734635f49bb05ce54e7709da8469cd2d7fc
David Archanjo <david.archanjo.ext@dasa.com.br>
2021-12-30 01:28:33 -0300

    first commit

--- spring-beans/src/main/java/io/davidarchanjo/code/SpringBeansApplication.java
+++ spring-beans/src/main/java/io/davidarchanjo/code/SpringBeansApplication.java
@@ -1,13 +1,9 @@
 package io.davidarchanjo.code;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SpringBeansApplication {
 
     public static void main(String... args) {
-        SpringApplication.run(SpringBeansApplication.class, args);
+        // https://www.baeldung.com/spring-application-context
     }
 
 }
