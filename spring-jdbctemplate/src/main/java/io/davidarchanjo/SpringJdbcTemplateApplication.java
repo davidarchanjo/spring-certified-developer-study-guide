bb6d21e57d3f12c4f1725c4c1161636fb731e1c5
David Archanjo <david.archanjo.ext@dasa.com.br>
2022-01-21 08:38:28 -0300

    first commit

--- spring-jdbctemplate/src/main/java/io/davidarchanjo/SpringJdbcTemplateApplication.java
+++ spring-jdbctemplate/src/main/java/io/davidarchanjo/SpringJdbcTemplateApplication.java
@@ -48,14 +48,4 @@ public class SpringJdbcTemplateApplication {
 		};
 	}
 }
-
-@RestController
-class Hello {
-
-    @RequestMapping("/")
-    String index() {
-        return "Hello world";
-    }
-}
-
 // docker run --name spring-jdbc-postgres --rm -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e PGDATA=/var/lib/postgresql/data/pgdata -v /tmp:/var/lib/postgresql/data -p 5432:5432 -it postgres:14.1-alpine
