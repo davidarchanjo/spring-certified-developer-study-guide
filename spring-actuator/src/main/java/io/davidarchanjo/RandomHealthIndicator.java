a91733f220b42119d5b6dcc3bce67e76bc94ae9e
David Archanjo <david.archanjo.ext@dasa.com.br>
2022-02-26 07:10:22 -0300

    first commit

---
+++ spring-actuator/src/main/java/io/davidarchanjo/RandomHealthIndicator.java
@@ -0,0 +1,21 @@
+package io.davidarchanjo;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+public class RandomHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        double chance = ThreadLocalRandom.current().nextDouble();
+        Health.Builder status = (chance > 0.9) ? Health.down() : Health.up();
+        return status
+            .withDetail("chance", chance)
+            .withDetail("strategy", "thread-local")
+            .build();
+    }
+}
