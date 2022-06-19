1b7cf5c4a59a7352cd7b6e922aa8754bc683e70e
David Archanjo <david.archanjo.ext@dasa.com.br>
2022-06-19 14:35:59 -0300

    first commit

--- spring-data-jpa/src/main/java/io/davidarchanjo/SpringDataJpaApplication.java
+++ spring-data-jpa/src/main/java/io/davidarchanjo/SpringDataJpaApplication.java
@@ -1,10 +1,8 @@
 package io.davidarchanjo;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
